9d08d0bc981bd5065b2c7d2041de2ca79241ee06
Pierre Kestener <pierre.kestener@cea.fr>
2026-04-03 18:52:34 +0200

    Implement Mie-Gruneisen mixture relations (two-fluid version).
    Multi-fluid version is coming.

---
+++ src/kalypsso/core/eos/MieGruneisenMixture.cpp
@@ -0,0 +1,330 @@
+// SPDX-FileCopyrightText: 2025 kalypsso-core authors
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+/**
+ * \file MieGruneisenMixture.cpp
+ */
+#include <kalypsso/core/eos/MieGruneisenMixture.h>
+#include <kalypsso/utils/log/kalypsso_log.h>
+
+namespace kalypsso
+{
+
+namespace core
+{
+
+namespace eos
+{
+
+// =====================================================================
+// =====================================================================
+int
+get_number_of_eos(ConfigMap const & config_map, MG_EOS_TYPE mg_eos_type)
+{
+  int        num_mat = 0;
+  const auto nmat = static_cast<size_t>(config_map.getInteger("run", "nmat", 2));
+
+  for (size_t imat = 0; imat < nmat; ++imat)
+  {
+    if (get_mg_eos_type(imat, config_map) == mg_eos_type)
+    {
+      num_mat++;
+    }
+  }
+
+  return num_mat;
+} // get_number_of_eos
+
+// =====================================================================
+// =====================================================================
+template <typename device_t>
+MieGruneisenMixture<device_t>::MieGruneisenMixture(ConfigMap const & config_map)
+  : m_num_material(static_cast<size_t>(config_map.getInteger("run", "nmat", 2)))
+  , m_mg_eos_ig(get_number_of_eos(config_map, MG_EOS_TYPE::MG_IDEAL_GAS))
+  , m_mg_eos_sg(get_number_of_eos(config_map, MG_EOS_TYPE::MG_STIFFENED_GAS))
+  , m_mg_eos_vdw(get_number_of_eos(config_map, MG_EOS_TYPE::MG_VANDERWAALS_GAS))
+  , m_mg_eos_sw(get_number_of_eos(config_map, MG_EOS_TYPE::MG_SHOCKWAVE))
+  , m_mg_eos_cc(get_number_of_eos(config_map, MG_EOS_TYPE::MG_COCHRAN_CHAN))
+  , m_mg_eos_jwl(get_number_of_eos(config_map, MG_EOS_TYPE::MG_JWL))
+  , m_material_eos_type("material_eos_type", m_num_material)
+  , m_material_eos_id("material_eos_id", m_num_material)
+{
+  // EOS histogram
+  auto eos_histogram = Kokkos::View<int *, HostDevice>("EOS histogram", MG_EOS_TYPE::MG_NUM);
+  Kokkos::deep_copy(eos_histogram, 0);
+
+  auto material_eos_type_host =
+    DataArray<MG_EOS_TYPE, device_t>::create_host_mirror_view(m_material_eos_type);
+  auto material_eos_id_host = DataArray<int, device_t>::create_host_mirror_view(m_material_eos_id);
+
+  for (size_t i = 0; i < m_num_material; ++i)
+    material_eos_id_host(i) = 0;
+
+  // init material eos type array (on host)
+  for (size_t imat = 0; imat < m_num_material; ++imat)
+  {
+    const auto eos_type = get_mg_eos_type(imat, config_map);
+    assertm(eos_type != +MG_EOS_TYPE::MG_INVALID,
+            "Wrong Mie-Gruneisen eos type. Please check input file.");
+    material_eos_type_host(imat) = eos_type;
+    material_eos_id_host(imat) = eos_histogram(eos_type);
+    eos_histogram(eos_type) += 1;
+  }
+
+  // monitoring
+  KALYPSSO_INFO("Mie-Gruneisen mixture");
+  for (size_t imat = 0; imat < m_num_material; ++imat)
+  {
+    KALYPSSO_INFO("material #{} eos={} id={}",
+                  imat,
+                  material_eos_type_host(imat)._to_string(),
+                  material_eos_id_host(imat));
+  }
+
+  // copy to device
+  Kokkos::deep_copy(m_material_eos_id, material_eos_id_host);
+  Kokkos::deep_copy(m_material_eos_type, material_eos_type_host);
+
+} // MieGruneisenMixture<device_t>::MieGruneisenMixture
+
+// =====================================================================
+// =====================================================================
+template <typename device_t>
+real_t
+MieGruneisenMixture<device_t>::material_gruneisen_param(int imat, real_t rho) const
+{
+  // get EOS type
+  const auto eos_type = m_material_eos_type(imat);
+
+  // get material eos id
+  const auto material_eos_id = m_material_eos_id(imat);
+
+  if (eos_type == +MG_EOS_TYPE::MG_IDEAL_GAS)
+  {
+    return m_mg_eos_ig(material_eos_id).gamma(rho);
+  }
+  else if (eos_type == +MG_EOS_TYPE::MG_STIFFENED_GAS)
+  {
+    return m_mg_eos_sg(material_eos_id).gamma(rho);
+  }
+  else if (eos_type == +MG_EOS_TYPE::MG_VANDERWAALS_GAS)
+  {
+    return m_mg_eos_vdw(material_eos_id).gamma(rho);
+  }
+  else if (eos_type == +MG_EOS_TYPE::MG_SHOCKWAVE)
+  {
+    return m_mg_eos_sw(material_eos_id).gamma(rho);
+  }
+  else if (eos_type == +MG_EOS_TYPE::MG_JWL)
+  {
+    return m_mg_eos_jwl(material_eos_id).gamma(rho);
+  }
+  else if (eos_type == +MG_EOS_TYPE::MG_COCHRAN_CHAN)
+  {
+    return m_mg_eos_cc(material_eos_id).gamma(rho);
+  }
+
+} // MieGruneisenMixture<device_t>::material_gruneisen_param
+
+// =====================================================================
+// =====================================================================
+template <typename device_t>
+real_t
+MieGruneisenMixture<device_t>::material_eint_ref(int imat, real_t rho) const
+{
+  // get EOS type
+  const auto eos_type = m_material_eos_type(imat);
+
+  // get material eos id
+  const auto material_eos_id = m_material_eos_id(imat);
+
+  if (eos_type == +MG_EOS_TYPE::MG_IDEAL_GAS)
+  {
+    return m_mg_eos_ig(material_eos_id).eint_ref(rho);
+  }
+  else if (eos_type == +MG_EOS_TYPE::MG_STIFFENED_GAS)
+  {
+    return m_mg_eos_sg(material_eos_id).eint_ref(rho);
+  }
+  else if (eos_type == +MG_EOS_TYPE::MG_VANDERWAALS_GAS)
+  {
+    return m_mg_eos_vdw(material_eos_id).eint_ref(rho);
+  }
+  else if (eos_type == +MG_EOS_TYPE::MG_SHOCKWAVE)
+  {
+    return m_mg_eos_sw(material_eos_id).eint_ref(rho);
+  }
+  else if (eos_type == +MG_EOS_TYPE::MG_JWL)
+  {
+    return m_mg_eos_jwl(material_eos_id).eint_ref(rho);
+  }
+  else if (eos_type == +MG_EOS_TYPE::MG_COCHRAN_CHAN)
+  {
+    return m_mg_eos_cc(material_eos_id).eint_ref(rho);
+  }
+
+} // MieGruneisenMixture<device_t>::material_eint_ref
+
+// =====================================================================
+// =====================================================================
+template <typename device_t>
+real_t
+MieGruneisenMixture<device_t>::material_pressure_ref(int imat, real_t rho) const
+{
+  // get EOS type
+  const auto eos_type = m_material_eos_type(imat);
+
+  // get material eos id
+  const auto material_eos_id = m_material_eos_id(imat);
+
+  if (eos_type == +MG_EOS_TYPE::MG_IDEAL_GAS)
+  {
+    return m_mg_eos_ig(material_eos_id).pressure_ref(rho);
+  }
+  else if (eos_type == +MG_EOS_TYPE::MG_STIFFENED_GAS)
+  {
+    return m_mg_eos_sg(material_eos_id).pressure_ref(rho);
+  }
+  else if (eos_type == +MG_EOS_TYPE::MG_VANDERWAALS_GAS)
+  {
+    return m_mg_eos_vdw(material_eos_id).pressure_ref(rho);
+  }
+  else if (eos_type == +MG_EOS_TYPE::MG_SHOCKWAVE)
+  {
+    return m_mg_eos_sw(material_eos_id).pressure_ref(rho);
+  }
+  else if (eos_type == +MG_EOS_TYPE::MG_JWL)
+  {
+    return m_mg_eos_jwl(material_eos_id).pressure_ref(rho);
+  }
+  else if (eos_type == +MG_EOS_TYPE::MG_COCHRAN_CHAN)
+  {
+    return m_mg_eos_cc(material_eos_id).pressure_ref(rho);
+  }
+
+} // MieGruneisenMixture<device_t>::material_pressure_ref
+
+// =====================================================================
+// =====================================================================
+template <typename device_t>
+real_t
+MieGruneisenMixture<device_t>::mixture_gruneisen_param(real_t phi0,
+                                                       real_t phi1,
+                                                       real_t phi_rho0,
+                                                       real_t phi_rho1) const
+{
+  real_t tmp = ZERO_F;
+
+  // material 0
+  if (phi0 > LOW_PHI)
+  {
+    tmp += phi0 / material_gruneisen_param(0, phi_rho0 / phi0);
+  }
+
+  // material 1
+  if (phi1 > LOW_PHI)
+  {
+    tmp += phi1 / material_gruneisen_param(0, phi_rho1 / phi1);
+  }
+
+  return 1 / tmp;
+
+} // MieGruneisenMixture<device_t>::mixture_gruneisen_param
+
+// =====================================================================
+// =====================================================================
+template <typename device_t>
+real_t
+MieGruneisenMixture<device_t>::mixture_pressure(real_t rho,
+                                                real_t eint,
+                                                real_t phi0,
+                                                real_t phi1,
+                                                real_t phi_rho0,
+                                                real_t phi_rho1) const
+{
+  // use eq (34) from Wallis et all, A unified diffuse interface method for the interaction
+  // of rigid bodies with elastoplastic solids and multi-phase mixtures.
+  // J. Appl. Phys. 131, 104901 (2022)
+  // https://doi.org/10.1063/5.0079970
+
+  real_t tmp = ZERO_F;
+
+  // material 0
+  if (phi0 > LOW_PHI)
+  {
+    const auto rho0 = phi_rho0 / phi0;
+    tmp += phi_rho0 * material_eint_ref(0, rho0) -
+           phi0 * material_pressure_ref(0, rho0) / material_gruneisen_param(0, rho0);
+  }
+
+  // material 1
+  if (phi1 > LOW_PHI)
+  {
+    const auto rho1 = phi_rho1 / phi1;
+    tmp += phi_rho1 * material_eint_ref(1, rho1) -
+           phi1 * material_pressure_ref(1, rho1) / material_gruneisen_param(1, rho1);
+  }
+
+  const auto pressure_mix =
+    mixture_gruneisen_param(phi0, phi1, phi_rho0, phi_rho1) * (rho * eint - tmp);
+
+  return pressure_mix;
+
+} // MieGruneisenMixture<device_t>::mixture_pressure
+
+// =====================================================================
+// =====================================================================
+template <typename device_t>
+real_t
+MieGruneisenMixture<device_t>::mixture_specific_eint(real_t rho,
+                                                     real_t pressure,
+                                                     real_t phi0,
+                                                     real_t phi1,
+                                                     real_t phi_rho0,
+                                                     real_t phi_rho1) const
+{
+  // use eq (33) from Wallis et all, A unified diffuse interface method for the interaction
+  // of rigid bodies with elastoplastic solids and multi-phase mixtures.
+  // J. Appl. Phys. 131, 104901 (2022)
+  // https://doi.org/10.1063/5.0079970
+
+  real_t eint = ZERO_F;
+
+  // material 0
+  if (phi0 > LOW_PHI)
+  {
+    const auto rho0 = phi_rho0 / phi0;
+    eint += phi_rho0 * material_eint_ref(0, rho0);
+    eint += phi0 * (pressure - material_pressure_ref(0, rho0)) / material_gruneisen_param(0, rho0);
+  }
+
+  // material 1
+  if (phi1 > LOW_PHI)
+  {
+    const auto rho1 = phi_rho1 / phi1;
+    eint += phi_rho1 * material_eint_ref(1, rho1);
+    eint += phi1 * (pressure - material_pressure_ref(1, rho1)) / material_gruneisen_param(1, rho1);
+  }
+
+  return eint;
+
+} // MieGruneisenMixture<device_t>::mixture_specific_eint
+
+// =====================================================================
+// =====================================================================
+template <typename device_t>
+real_t
+MieGruneisenMixture<device_t>::mixture_volumic_eint(real_t rho,
+                                                    real_t pressure,
+                                                    real_t phi0,
+                                                    real_t phi1,
+                                                    real_t phi_rho0,
+                                                    real_t phi_rho1) const
+{
+  return rho * mixture_volumic_eint(rho, pressure, phi0, phi1, phi_rho0, phi_rho1);
+} // MieGruneisenMixture<device_t>::mixture_volumic_eint
+
+} // namespace eos
+} // namespace core
+} // namespace kalypsso
---
+++ src/kalypsso/core/eos/MieGruneisenMixture.h
@@ -0,0 +1,277 @@
+// SPDX-FileCopyrightText: 2026 kalypsso-core authors
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+/**
+ * \file MieGruneisenMixture.h
+ *
+ * This is only useful when using at least two material to define pressure and
+ * internal energy in mixed cells.
+ *
+ * Reference for mixture rules (isobaric closure):
+ *
+ * Law and Barton, A cell centered volume-of-fluid method for compressible
+ * multi-material flows. Journal of Computational Physics, 497 (2024) 112592.
+ * https://doi.org/10.1016/j.jcp.2023.112592
+ */
+#ifndef KALYPSSO_CORE_EOS_MIE_GRUNEISEN_MIXTURE_H_
+#define KALYPSSO_CORE_EOS_MIE_GRUNEISEN_MIXTURE_H_
+
+#include <kalypsso/core/kokkos_shared.h>
+#include <kalypsso/core/real_type.h>
+#include <kalypsso/utils/config/ConfigMap.h>
+#include <kalypsso/core/eos/eos_utils.h>
+#include <kalypsso/core/DataArray.h>
+
+#include <kalypsso/core/eos/MieGruneisenEosCochranChan.h>
+#include <kalypsso/core/eos/MieGruneisenEosJWL.h>
+#include <kalypsso/core/eos/MieGruneisenEosSW.h>
+#include <kalypsso/core/eos/MieGruneisenEosIdealGas.h>
+#include <kalypsso/core/eos/MieGruneisenEosStiffenedGas.h>
+#include <kalypsso/core/eos/MieGruneisenEosVanDerWaalsGas.h>
+
+
+namespace kalypsso
+{
+
+namespace core
+{
+
+namespace eos
+{
+
+//! type alias for an array of Mie-Gruneisen EOS of type Cochran-Chan
+template <typename device_t>
+using mg_eos_cc_array_t = Kokkos::View<MieGruneisenEosCochranChan *, device_t>;
+
+//! type alias for an array of Mie-Gruneisen EOS of type JWL
+template <typename device_t>
+using mg_eos_jwl_array_t = Kokkos::View<MieGruneisenEosJWL *, device_t>;
+
+//! type alias for an array of Mie-Gruneisen EOS of type SW (Shockwave)
+template <typename device_t>
+using mg_eos_sw_array_t = Kokkos::View<MieGruneisenEosSW *, device_t>;
+
+//! type alias for an array of Mie-Gruneisen EOS of type ideal gas
+template <typename device_t>
+using mg_eos_ig_array_t = Kokkos::View<MieGruneisenEosIdealGas *, device_t>;
+
+//! type alias for an array of Mie-Gruneisen EOS of type stiffened gas
+template <typename device_t>
+using mg_eos_sg_array_t = Kokkos::View<MieGruneisenEosStiffenedGas *, device_t>;
+
+//! type alias for an array of Mie-Gruneisen EOS of type van der Waals gas
+template <typename device_t>
+using mg_eos_vdw_array_t = Kokkos::View<MieGruneisenEosVanDerWaalsGas *, device_t>;
+
+//! return the number of material of given Mie-Gruneisen type
+int
+get_number_of_eos(ConfigMap const & config_map, MG_EOS_TYPE mg_eos_type);
+
+
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================
+/**
+ * Mie-Gruneisen mixture (isobaric closure) helper class.
+ *
+ * In a multifluid simulation, when each fluid obeys an EOS of Mie-Gruneisen type,
+ * one needs to define mixture rule to give an averaged (mixed) thermodynamical states
+ * in mixed cells, i.e. where more than 1 materials are present.
+ *
+ * Mie-Gruneisen EOS for material \f$ i \f$
+ *
+ * \f$ p_i = p_{i,\text{ref}} + \rho_i \Gamma_i \left( \mathcal{E}_i - \mathcal{E}_{i,\text{ref}}
+ * \right)\f$
+ *
+ * Main mixture relations are:
+ *
+ * - volumic fraction conservation : \f$ \sum_i \phi_i = 1 \f$
+ * - density : \f$ \rho = \sum_i \phi_i \rho_i \f$
+ * - volumic internal energy : \f$ \rho \mathcal{E} = \sum_i $\phi_i \rho_i \mathcal{E}_i \f$
+ * - pressure : \f$ \forall i, p_i = p \f$
+ *
+ * The mixture Gruneisen parameter is defined as :
+ *
+ * \f$ \Gamma_{\text{mix}} = \frac{1}{\sum_i \frac{\phi_i}{\Gamma_i}} \f$
+ *
+ * where \f$ \phi_i \f$ is the volume fraction of material \f$ i \f$ and \f$ \Gamma_i \f$
+ * the Gruneisen parameter of material \f$ i \f$.
+ *
+ * The mixture isentropic bulk modulus is defined as :
+ *
+ * \f$ K_S = \frac{1}{\sum_i \frac{\phi_i}{\K_{S,i}} } \f$
+ *
+ * The mixture internal energy is given by:
+ *
+ * \f$ \rho \mathcal{E} = \sum_i $\phi_i \rho_i eint_i \f$
+ *
+ * The mixture pressure (assuming isobaric closure) is given by:
+ *
+ * \f$ p = \Gamma_{\text{mix}} \left( \rho \mathcal{E} - \sum_i \phi_i (\rho_i
+ * \mathcal{E}_{i,\text{ref}} - \frac{p_{i,\text{ref}}}{\Gamma_i}) \right)\f$
+ *
+ */
+template <typename device_t>
+class MieGruneisenMixture
+{
+
+public:
+  MieGruneisenMixture() = default;
+
+  MieGruneisenMixture(ConfigMap const & config_map);
+
+private:
+  //! number of materials
+  size_t m_num_material;
+
+  //! array of eos, one per material (Ideal gas)
+  mg_eos_ig_array_t<device_t> m_mg_eos_ig;
+
+  //! array of eos, one per material (stiffened gas)
+  mg_eos_sg_array_t<device_t> m_mg_eos_sg;
+
+  //! array of eos, one per material (van der waals gas)
+  mg_eos_vdw_array_t<device_t> m_mg_eos_vdw;
+
+  //! array of eos, one per material (shockwave)
+  mg_eos_sw_array_t<device_t> m_mg_eos_sw;
+
+  //! array of eos, one per material (Cochran-Chan)
+  mg_eos_cc_array_t<device_t> m_mg_eos_cc;
+
+  //! array of eos, one per material (JWL)
+  mg_eos_jwl_array_t<device_t> m_mg_eos_jwl;
+
+  //! material eos type array
+  DataArray<MG_EOS_TYPE, device_t> m_material_eos_type;
+
+  //! material eos id.
+  //! material id among all material with the same eos
+  DataArray<int, device_t> m_material_eos_id;
+
+  //! low value for a volume fraction
+  static constexpr real_t LOW_PHI = 1e-5;
+
+  /**
+   * Retrieve Gruneisen parameter of a given material.
+   *
+   * \param[in] imat material id
+   * \param[in] rho material density (rho_phi/phi)
+   *
+   * \note the caller is responsible for computing material density and cross-checking
+   * that the volume fraction is not too small
+   */
+  KOKKOS_FUNCTION
+  real_t
+  material_gruneisen_param(int imat, real_t rho) const;
+
+  /**
+   * Retrieve reference internal energy of a given material.
+   *
+   * \param[in] imat material id
+   * \param[in] rho material density (rho_phi/phi)
+   *
+   * \note the caller is responsible for computing material density and cross-checking
+   * that the volume fraction is not too small
+   */
+  KOKKOS_FUNCTION
+  real_t
+  material_eint_ref(int imat, real_t rho) const;
+
+  /**
+   * Retrieve reference pressure of a given material.
+   *
+   * \param[in] imat material id
+   * \param[in] rho material density (rho_phi/phi)
+   *
+   * \note the caller is responsible for computing material density and cross-checking
+   * that the volume fraction is not too small
+   */
+  KOKKOS_FUNCTION
+  real_t
+  material_pressure_ref(int imat, real_t rho) const;
+
+public:
+  /**
+   * Compute mixture Gruneisen parameter (two materials).
+   *
+   * \param[in] phi0 volume fraction of material 0
+   * \param[in] phi1 volume fraction of material 1
+   * \param[in] rho_phi0 partial density of material 0
+   * \param[in] rho_phi1 partial density of material 1
+   */
+  KOKKOS_FUNCTION
+  real_t
+  mixture_gruneisen_param(real_t phi0, real_t phi1, real_t phi_rho0, real_t phi_rho1) const;
+
+  /**
+   * Compute mixture pressure.
+   *
+   * To be used only when there are two materials
+   *
+   * \param[in] rho mixture density
+   * \param[in] eint mixture internal energy
+   * \param[in] phi0 volume fraction of material 0
+   * \param[in] phi1 volume fraction of material 1
+   * \param[in] rho_phi0 partial density of material 0
+   * \param[in] rho_phi1 partial density of material 1
+   */
+  KOKKOS_FUNCTION
+  real_t
+  mixture_pressure(real_t rho,
+                   real_t eint,
+                   real_t phi0,
+                   real_t phi1,
+                   real_t phi_rho0,
+                   real_t phi_rho1) const;
+
+  /**
+   * Compute mixture specific internal energy.
+   *
+   * To be used only when there are two materials
+   *
+   * \param[in] rho mixture density
+   * \param[in] eint mixture internal energy
+   * \param[in] phi0 volume fraction of material 0
+   * \param[in] phi1 volume fraction of material 1
+   * \param[in] rho_phi0 partial density of material 0
+   * \param[in] rho_phi1 partial density of material 1
+   */
+  KOKKOS_FUNCTION
+  real_t
+  mixture_specific_eint(real_t rho,
+                        real_t pressure,
+                        real_t phi0,
+                        real_t phi1,
+                        real_t phi_rho0,
+                        real_t phi_rho1) const;
+
+  /**
+   * Compute mixture volumic internal energy.
+   *
+   * To be used only when there are two materials
+   *
+   * \param[in] rho mixture density
+   * \param[in] eint mixture internal energy
+   * \param[in] phi0 volume fraction of material 0
+   * \param[in] phi1 volume fraction of material 1
+   * \param[in] rho_phi0 partial density of material 0
+   * \param[in] rho_phi1 partial density of material 1
+   */
+  KOKKOS_FUNCTION
+  real_t
+  mixture_volumic_eint(real_t rho,
+                       real_t pressure,
+                       real_t phi0,
+                       real_t phi1,
+                       real_t phi_rho0,
+                       real_t phi_rho1) const;
+
+}; // class MieGruneisenMixture
+
+} // namespace eos
+} // namespace core
+} // namespace kalypsso
+
+#endif // KALYPSSO_CORE_EOS_MIE_GRUNEISEN_MIXTURE_H_
